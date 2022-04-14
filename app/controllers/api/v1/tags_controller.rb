fe382f6e8c0e09af49f48189279eb5cc12585f7e
suixing <suixing@uni-ubi.com>
2022-04-14 11:41:15 +0800

    查询单个tag的单元测试完成

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::TagsController < ApplicationController
+ def create
+		render_resource Tag.create! tag_name: params[:tag_name], tag_icon: params[:tag_icon]
+ end
+
+	def index
+			 tag = Tag.page(params[:page]).per 10
+				render json: {resources: tag,pager: {
+					page: params[:page],
+					per_page: 10,
+					count: Tag.count
+				}}, status: 200
+	end
+
+	def show
+		render_resource Tag.find params[:tag_id]
+	end
+
 end
--- spec/requests/api/v1/tags_spec.rb
+++ spec/requests/api/v1/tags_spec.rb
@@ -1,9 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Tags", type: :request do
-  describe "create tag" do
+  describe "创建tag" do
     it "should create a tag" do
-      # post "/api/v1/tags", params: {}
+      post "/api/v1/tags", params: {tag_name: '公交', tag_icon:'imageSrc'}
+      expect(response).to have_http_status 200 
+      body = JSON.parse(response.body)
+      expect(body["resource"]["tag_name"]).to eq "公交"
+      expect(body['resource']["tag_icon"]).to eq "imageSrc"  
+    end
+  end
+
+  describe "查询所有（分页）的tag" do
+    it "should find all tag" do
+      11.times { Tag.create tag_name: '公交', tag_icon:'imageSrc'  }
+      get '/api/v1/tags'
+      expect(response).to have_http_status 200  
+      body = JSON.parse(response.body)
+      expect(body["resources"].size).to eq 10  
+      get '/api/v1/tags?page=2'
+      expect(response).to have_http_status 200  
+      body = JSON.parse(response.body)
+      expect(body["resources"].size).to eq 1
+    end
+  end
+
+  describe "查询单个的tag" do
+    it "should find the tag" do
+      tag = Tag.create! tag_name: "公交", tag_icon: 'imageSrc'
+      get "/api/v1/tags?tag_id=#{tag.id}"
+      expect(response).to have_http_status 200  
+    end
+
+    it "could not find the wrong tag" do
+      get "/api/v1/tags?tag_id=9999999999999999999"
+      body = JSON.parse(response.body)
+      expect(response).to have_http_status 200  
+      expect(body["resources"].size).to eq 0  
     end
   end
 end
